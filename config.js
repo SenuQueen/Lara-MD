da2c2cf5ec9f5b102faf6be739184c23e8221a17
SenuQueen <senurinimsara10@gmail.com>
2024-12-21 00:30:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'false') {
-    return text === fault ? true : false;
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : true;
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "GroQnCyA#Z78YQ8GenR88ws6NYCaqrw8cxdh46vL_ja66uJN_bGg", // Add your session id
