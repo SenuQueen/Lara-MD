563f2871da680c4f6a505bce060e61e4a85e14e7
SenuQueen <senurinimsara10@gmail.com>
2024-12-21 00:27:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'false') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2vhUkRSA#UtccCm5PPsd2UDD5e901MTDb7PkgS2F_oNLT8agjwzU", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "GroQnCyA#Z78YQ8GenR88ws6NYCaqrw8cxdh46vL_ja66uJN_bGg", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
