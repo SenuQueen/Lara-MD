ccee477f26ab9059c72750a56db6fc38f4b57f74
SenuQueen <senurinimsara10@gmail.com>
2024-12-21 00:36:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : true;
+function convertToBool(text, fault = 'false') {
+    return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GroQnCyA#Z78YQ8GenR88ws6NYCaqrw8cxdh46vL_ja66uJN_bGg", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "fy5nCThC#1_IG4OqtCuamR9JNP54sIlkR-fu-PiglLS4zty9-Ncw", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
